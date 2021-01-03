ecbccdc6a95dc35f87d80863960cfcbfa5966c35
atayl16 <atayl16@yahoo.com>
2021-01-03 17:29:50 +0100

    Dynamic routes for proposals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent} from './homepage/homepage.component';
-import { DocumentsModule } from './documents/documents.module';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
