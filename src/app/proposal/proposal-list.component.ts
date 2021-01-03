6ba052ea17563bde60602639985decea393bfd2a
atayl16 <atayl16@yahoo.com>
2021-01-03 17:19:56 +0100

    fix proposal list

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { Proposal } from ',/proposal';
+import { Proposal } from './proposal';
 
-@Component ({
-  selector: 'proposal-list',
-  templateUrl: 'proposal-list.component.html'
-
+@Component({
+	selector: 'proposal-list',
+	templateUrl: 'proposal-list.component.html'
 })
-
 export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://google.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
-	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://google.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
-	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://google.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://www.yahoo.com.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
+	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://www.yahoo.com.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
+	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://www.yahoo.com.com', 'Ruby on Rails', 150, 120, 15, 'test@example.com')
 
 	proposals: Proposal[] = [
 		this.proposalOne,
