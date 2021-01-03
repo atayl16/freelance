1e939e948ce1add30b858fa3c5024a12dff313c7
atayl16 <atayl16@yahoo.com>
2021-01-03 19:13:33 +0100

    added navbar

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,37 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class DocumentsComponent {
   pageTitle: string = "Document Dashboard"
   documents: Document[] = [
   		{
   			title: "My First Doc",
-  			description: 'asdfasdfasdf asdfasd',
-  			file_url: 'http://google.com',
-  			updated_at: '11/11/16',
-  			image_url: 'http://google.com',
+  			description: "asdfasdfasdf asdfasd",
+  			file_url: "http://google.com",
+  			updated_at: "11/11/16",
+  			image_url: "http://google.com",
   		},
   		{
   			title: "My Second Doc",
-  			description: 'asdfasdfasdf asdfasd',
-  			file_url: 'http://google.com',
-  			updated_at: '11/11/16',
-  			image_url: 'http://google.com',
+  			description: "asdfasdfasdf asdfasd",
+  			file_url: "http://google.com",
+  			updated_at: "11/11/16",
+  			image_url: "http://google.com",
   		},
   		{
   			title: "My Last Doc",
-  			description: 'asdfasdfasdf asdfasd',
-  			file_url: 'http://google.com',
-  			updated_at: '11/11/16',
-  			image_url: 'http://google.com',
+  			description: "asdfasdfasdf asdfasd",
+  			file_url: "http://google.com",
+  			updated_at: "11/11/16",
+  			image_url: "http://google.com",
   		}
   	]
 }
